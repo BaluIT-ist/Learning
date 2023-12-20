b96f497421f0ed90d728f14bf7ff72672b5d581f
dgadrian05 <dgadrian05@gmail.com>
2023-12-20 22:11:17 +0200

    Continuing the online course

--- First code/src/com/balu/Main.java
+++ First code/src/com/balu/Main.java
@@ -1,7 +1,19 @@
 package com.balu;
+
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+     double result = Math.random() *100;
+     if (result<50)
+     {
+         System.out.println("Your number is lower than 50");
+     }
+
+
+
     }
 }
